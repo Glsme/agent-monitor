b22b4e8a3e314823eb09498f3c5a9dfed8a5cc7e
SeokjuneHong <glasses.str.man@gmail.com>
2026-02-23 23:10:10 +0900

    chore: release version bump to 0.3.0

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -6,7 +6,7 @@
 
 $ErrorActionPreference = "Stop"
 
-$VERSION = "0.2.0"
+$VERSION = "0.3.0"
 $REPO = "Glsme/agent-monitor"
 $REPO_URL = "https://github.com/$REPO.git"
 $RELEASE_URL = "https://github.com/$REPO/releases/download/v$VERSION"
